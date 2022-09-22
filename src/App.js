0436255d18618e1f2b7a307cd8aebc9e37b8b078
Abhishek <abhishekparmar162@gmail.com>
2022-09-23 00:26:38 +0530

    added started files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {Route , Routes} from 'react-router-dom'
+import {Box} from '@mui/material'
+import NavBar from './componets/NavBar'
+
+import './App.css'
+import ExerciseDetails from './pages/ExerciseDetails';
+import Home from './pages/Home';
+import Footer from './componets/Footer';
 
 function App() {
   return (
-    <div className="App">
-     hey
-    </div>
+    <Box width='400px' sx={{width:{xl:'1448px'}}} m='auto'>
+     <NavBar />
+     <Footer />
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/exercise/:id' element={<ExerciseDetails />} />
+     
+     </Routes>
+    </Box>
   );
 }
 
